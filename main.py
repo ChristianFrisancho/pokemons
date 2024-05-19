37431cada4a6128c53ba5575a7847c1a999844fd
ChristianFrisancho <167444681+ChristianFrisancho@users.noreply.github.com>
2024-05-18 21:10:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
 import mysql.connector
 import schemas
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 host_name = "database-1.cpxnwinne8ao.us-east-1.rds.amazonaws.com"
 port_number = "3306"
